ae0485b75158cedc1a3a7a71584ac3311fdbd0ec
mr0rng <mr0rng15926@gmail.com>
2022-03-07 11:49:43 +0200

    Merge

---
+++ modules/api-contract/src/user/telegram/index.ts
@@ -0,0 +1,17 @@
+import { UserGetResponse } from '..';
+
+export interface ApiContractUserTelegram {
+  get: (request: TelegramUserGetRequest) => Promise<UserGetResponse>;
+  signUp: (request: TelegramUserSignUpRequest) => Promise<UserGetResponse>;
+}
+
+export type TelegramUserGetRequest = {
+  session: string;
+  telegramId: string;
+};
+
+export type TelegramUserSignUpRequest = {
+  session: string;
+  telegramId: string;
+  login: string;
+};
---
+++ services/api/tests/user/telegram/sign-up.test.ts
@@ -0,0 +1,43 @@
+import { TestsEnv } from '../..';
+
+const env = new TestsEnv();
+
+beforeEach(() => env.start());
+afterEach(() => env.stop());
+
+test('main', async () => {
+  const user = await env.client.user.telegram.signUp({
+    session: env.config.sessions.admin_key,
+    login: 'usrlgn',
+    telegramId: 'telegram-id',
+  });
+
+  expect(user).toStrictEqual({
+    isAdmin: false,
+    login: 'usrlgn',
+    type: 'telegram',
+    payload: { telegramId: 'telegram-id' },
+  });
+});
+
+test('duplicate web', async () => {
+  await env.dao.prepareDB({
+    Users: [
+      {
+        id: 'asd',
+        isAdmin: false,
+        isBanned: false,
+        login: 'usrlgn',
+        type: 'web',
+        payload: { privateKeys: { passwordHash: 'hshpwd' } },
+      },
+    ],
+  });
+
+  const signUpRequest = {
+    session: env.config.sessions.admin_key,
+    login: 'usrlgn',
+    telegramId: 'telegram-id',
+  };
+  await expect(env.client.user.telegram.signUp(signUpRequest)).rejects.toThrow('User already exists');
+});
